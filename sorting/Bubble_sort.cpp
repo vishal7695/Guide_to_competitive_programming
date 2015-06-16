8fa66f67737036be9dbd15c6631530d20d2c5210
vishal0408sharma <vishal0408sharma@gmail.com>
2015-06-16 20:26:55 +0530

    Bubble_sort

---
+++ sorting/Bubble_sort.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+
+using namespace std;
+
+void bubble_sort(int a[], int n)
+{
+    int i,j;
+	
+	// for n-1 passes
+    for(i=n-1;i>0;i--)
+    {
+		//consecutive elements will be compared
+        for(j=0;j<i;j++)
+        {
+			//swap if (j+1)th element is greater than jth element
+            if(a[j]>a[j+1])
+            {
+				//swap 
+                a[j]=a[j]+a[j+1];
+                a[j+1]=a[j]-a[j+1];
+                a[j]=a[j]-a[j+1];
+            }
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    cout<<"enter the no. of elements you want to insert";
+    cin>>n;
+
+    int a[15];
+
+    cout<<"enter the values";
+
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+
+    bubble_sort(a,n);
+
+    cout<<"The sorted array is "<<endl;
+
+    for(int i=0;i<n;i++)
+    {
+        cout<<a[i]<<" ";
+    }
+
+    return 0;
+}
