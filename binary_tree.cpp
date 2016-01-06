a99c297387ee7501de15eefd66ce6103b35b0f62
vishal0408sharma <vishal0408sharma@gmail.com>
2016-01-06 20:28:27 +0530

    updated complete binary tree operations

--- binary_tree.cpp
+++ binary_tree.cpp
@@ -1,4 +1,5 @@
-//binary tree operations
+//Complete binary tree operations
+
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -48,6 +49,16 @@ node *newNode()
     return n;
 }
 
+node *newNode(int x)
+{
+    node *n=new node;
+    n->left=NULL;
+    n->right=NULL;
+    n->data=x;
+
+    return n;
+}
+
 void create(node* &root)
 {
     int i,j,k,n;
@@ -61,8 +72,8 @@ void create(node* &root)
     root=newNode();
     q.push(root);
 
-    i=1;
-    while(i<n)
+    i=2;
+    while(i<=n)
     {
         node *temp=q.front();
         if(!temp->left)
@@ -317,12 +328,11 @@ void inorderIterative(node* &root)
    }
 }
 
-void search(node* &root,int x)
+bool search(node* &root,int x)
 {
     if(root==NULL)
     {
-        cout<<"empty tree"<<endl;
-        return ;
+        return false;
     }
 
     queue<node*> q;
@@ -331,17 +341,14 @@ void search(node* &root,int x)
     node *temp;
 
     bool found=false;
-    while(found==false && !q.empty())
+    while(!q.empty())
     {
         temp=q.front();
         q.pop();
 
         if(temp->data==x)
         {
-            cout<<"element found"<<endl;
-            found=true;
-
-            return ;
+            return true;
         }
 
         if(temp->left)
@@ -352,10 +359,7 @@ void search(node* &root,int x)
 
     }
 
-    if(found==false)
-    {
-        cout<<"element not found"<<endl;
-    }
+    return false;
 }
 
 
@@ -503,7 +507,198 @@ void deleteTreeIterative(node* &root)
 
 void levelWiseDisplay(node* &root)
 {
+    if(root==NULL)
+        return; 
 
+    //let us consider a node with value equal to -1 as unique node to be used as a level separator
+
+    node *unique=newNode(-1);
+
+    queue<node*> q;
+    q.push(root);
+    q.push(unique);
+
+    while(!q.empty())
+    {
+        if(q.front()->data==-1)
+        {
+            //unique symbol
+            cout<<endl;
+
+            q.pop();
+            if(!q.empty())
+                q.push(unique);
+        }
+
+        else
+        {
+            if(q.front()->left)
+                q.push(q.front()->left);
+
+            if(q.front()->right)
+                q.push(q.front()->right);
+
+            cout<<q.front()->data<<" ";
+            q.pop();
+        }
+    }
+
+}
+
+int height(node* &root)
+{
+    if(!root)
+        return 0;
+
+    return 1+max(height(root->left),height(root->right));
+}
+
+int heightWrapper(node *&root)
+{
+    if(!root)
+        return 0;
+    
+    if(!root->left && !root->right)
+        return 0;
+
+    return max(height(root->left),height(root->right));
+}
+
+bool isFullNode(node* &root)
+{
+    if(root->left && root->right)
+        return true;
+
+    return false;
+}
+
+int diameter(node* root)
+{
+    if(!root)
+        return 0;
+
+    int x=diameter(root->left);
+    int y=diameter(root->right);
+
+    int z;
+
+    if(isFullNode(root))
+        z=height(root->left)+1+height(root->right);
+
+    else
+        z=1;
+
+    return max(x,max(y,z));
+}
+
+void printRootToLeafPaths(node* &root,vector<int> path,int pathLen)
+{
+    if(!root)
+        return ;
+
+    //add this element to the path
+    path.push_back(root->data);
+    pathLen++;
+
+    //if this is a leaf node
+    if(!root->left && !root->right)
+    {
+        //print the path
+        for(int i=0;i<pathLen;i++)
+            cout<<path[i]<<" ";
+
+        cout<<endl;
+    }
+
+    //display other paths
+    printRootToLeafPaths(root->left,path,pathLen);
+    printRootToLeafPaths(root->right,path,pathLen);
+}
+
+
+bool checkSumPath(node* &root,int sum)
+{
+    if(!root)
+        return false;
+
+    sum=sum-root->data;
+
+    if(sum==0)
+    {
+        //path found
+        return true;
+    }
+
+    //check remaining paths
+    if(checkSumPath(root->left,sum))
+        return true;
+
+    else 
+        return checkSumPath(root->right,sum);
+
+}
+
+bool findPath(node* &root, int x, vector<int> &pathX, int lenX)
+{
+    if(!root)
+        return false;
+
+    //add this to path
+    if(pathX.size()>lenX)
+    {
+        pathX[lenX]=root->data;
+        lenX++;
+    }
+    
+    else
+    {
+        pathX.push_back(root->data);
+        lenX++;
+    }
+
+    if(root->data==x)
+    {
+        //erase extra elements in the path 
+        //the required element is at the index lenX-1
+        //so delete remaining elements (if there)
+
+        if(pathX.size()>lenX)
+            pathX.erase(pathX.begin()+(lenX),pathX.end());
+        
+        return true;
+    }
+
+
+    //search in the children
+    bool found=findPath(root->left,x,pathX,lenX);
+
+    if(found)
+        return true;
+
+    return findPath(root->right,x,pathX,lenX);
+}
+
+int lca(node* &root,int x, int y)
+{
+    if(!root)
+        return -1;
+
+    vector<int> pathX;
+    vector<int> pathY;
+
+    if(!findPath(root,x,pathX,0))
+        return -1;
+    
+    if(!findPath(root,y,pathY,0))
+        return -1;
+    
+    //now, we have the paths of x and y 
+    //compare 
+    int length=min(pathX.size(),pathY.size());
+
+    for(int i=length-1;i>=0;i--)
+        if(pathX[i]==pathY[i])
+            return pathX[i];
 }
 
 
@@ -511,6 +706,9 @@ int main()
 {
     node* root=NULL;
     int x,y;
+    int sum;
+    int a,b;
+    vector<int> path;
 
     do
     {
@@ -527,8 +725,15 @@ int main()
         cout<<" 10 delete an element"<<endl;
         cout<<" 11 delete tree iterative"<<endl;
         cout<<" 12 delete tree"<<endl;
+        cout<<" 13 Level Wise Display"<<endl;
+        cout<<" 14 height of tree"<<endl;
+        cout<<" 15 Diameter of tree"<<endl;
+        cout<<" 16 Print root to leaf paths"<<endl;
+        cout<<" 17 Check the existence of a path from root to any of the nodes with the given sum"<<endl;
+        cout<<" 18 Find path of a node from the root"<<endl;
+        cout<<" 19 Least Common Ancestor of two nodes"<<endl;
 
-        cout<<" 15 exit"<<endl;
+        cout<<" 20 exit"<<endl;
 
         cout<<endl<<"Enter your choice  ";
         in(x);
@@ -567,8 +772,13 @@ int main()
                 break;
 
             case 9:cout<<"enter the value of node to be searched  ";
-                   in(y);
-                   search(root,y);
+                   cin>>y;
+                   if(search(root,y))
+                       cout<<"Element found";
+                   else
+                       cout<<"Element not found";
+
+                   cout<<endl;
                    break;
 
             case 10:cout<<"enter the value of node to be deleted  ";
@@ -584,14 +794,71 @@ int main()
                     deleteTree(root);
                     break;
 
-            case 15:cout<<"exiting..."<<endl;
+            case 13:cout<<"Level wise display"<<endl;
+                    levelWiseDisplay(root);
+                    break;
+
+            case 14:cout<<"height of the tree"<<endl;
+                    cout<<heightWrapper(root);
+                    break;
+
+            case 15:cout<<"diameter of the tree"<<endl;
+                    cout<<diameter(root);
+                    break;
+            
+            case 16:cout<<"All the root to leaf paths in the tree are"<<endl;
+                    printRootToLeafPaths(root,path,0);
+                    path.clear();
+                    break;
+
+            case 17:cout<<"Enter the sum whose path you want to find in the tree"<<endl;
+                    cin>>sum;
+                    if(checkSumPath(root,sum))
+                        cout<<"Path with required sum found";
+                    else
+                        cout<<"No path found for given sum";
+
+                    cout<<endl;
+                    break;
+
+            case 18:cout<<"Enter the value of node whose path you want to find"<<endl;
+                    cin>>a;
+                    if(!findPath(root,a,path,0))
+                        cout<<"No such element found";
+
+                    else
+                    {
+                        //display path
+                        cout<<"The path of required node is "<<endl;
+                        for(b=0;b<path.size();b++)
+                            cout<<path[b]<<" ";
+
+                        cout<<endl;
+                        path.clear();
+                    }
+
+                    break;
+
+            case 19:cout<<"Enter the value of two nodes whose LCA you want to find"<<endl;
+                    cin>>a>>b;
+                    cout<<"Least Common Ancestor of the given numbers is ";
+                    y=lca(root,a,b);
+                    if(y!=-1)
+                        cout<<y;
+                    else
+                        cout<<"Not Found";
+
+                    break;
+
+
+            case 20:cout<<"exiting..."<<endl;
                 break;
 
             default:cout<<"invalid option...please try again"<<endl;
 
         }
 
-    }while(x!=15);
+    }while(x!=20);
 
     return 0;
 }
